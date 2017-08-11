0a7f6453a41c5791a6215d98f655d5028bbee882
zyy <zyy136807@antfin.com>
2017-08-11 13:54:42 +0800

    .

--- js/test.js
+++ js/test.js
@@ -1,11 +1,8 @@
-/* 
-* @Author: Zhang Yingya
-* @Date:   2016-02-23 14:12:27
-* @Last Modified by:   zyy
-* @Last Modified time: 2016-02-25 22:20:16
-*/
-ﬁ
-'use strict';
-var a;
-var b;
+const arr = [
+  "asdfasdfasd",
+  "asdfasdfasdf",
+  "asdfasdfasdf",
+  "asdfasdfasdfasdfasd",
+];
 
+arr.sort();
